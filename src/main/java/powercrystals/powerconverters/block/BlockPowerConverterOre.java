dc6b2a3087290d02b92c7b5b512277ef87c2ec7b
covers1624 <laughlan.cov@internode.on.net>
2014-11-10 18:36:04 +1030

    Ores

---
+++ src/main/java/powercrystals/powerconverters/block/BlockPowerConverterOre.java
@@ -0,0 +1,19 @@
+package powercrystals.powerconverters.block;
+
+import powercrystals.powerconverters.gui.PCCreativeTab;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockPowerConverterOre extends Block{
+
+	protected BlockPowerConverterOre() {
+		super(Material.rock);
+		setHardness(3.0F);
+		setResistance(5.0F);
+		setStepSound(soundTypePiston);
+		setCreativeTab(PCCreativeTab.tab);
+		setBlockName("powerconverters.ore");
+		
+		setHarvestLevel("pickaxe", 2);
+	}
+}
