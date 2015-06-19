26a47a3259a82d23a159764c9993e800bc6cab84
covers1624 <laughlan.cov@internode.on.net>
2015-06-19 16:58:21 +0930

    Build Pushed!

--- src/main/java/powercrystals/powerconverters/reference/Reference.java
+++ src/main/java/powercrystals/powerconverters/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	// Mod Info.
 	public static final String MOD_ID = "PowerConverters";
 	public static final String MOD_NAME = "Power Converters";
-	public static final String MOD_VERSION = "1.7.10-2.6";
+	public static final String MOD_VERSION = "1.7.10-2.7";
 
 	// Texture Info
 	public static final String MOD_PREFIX = MOD_ID.toLowerCase() + ":";
